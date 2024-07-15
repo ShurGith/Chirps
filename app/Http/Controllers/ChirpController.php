79786c931f8b592f8136266ce9257f970be14d57
ShurGith <75266693+ShurGith@users.noreply.github.com>
2024-07-15 19:45:59 +0200

    Inicio

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+//use Illuminate\Contracts\View\View;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Http\Redirectresponse;
+use Illuminate\Support\Facades\Gate;
+class ChirpController extends Controller
+{
+    public function index(): View
+    {
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
+    }
+    public function create()
+    {
+        //
+    }
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+    public function edit(Chirp $chirp): View
+    {
+        //
+        Gate::authorize('update', $chirp);
+
+        return view('chirps.edit', [
+            'chirp' => $chirp,
+        ]);
+    }
+
+    public function update(Request $request, Chirp $chirp): RedirectResponse
+    {
+        Gate::authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
+    }
+    public function destroy(Chirp $chirp): RedirectResponse
+    {
+        Gate::authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Chirp;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
@@ -43,5 +44,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
     }
 }
